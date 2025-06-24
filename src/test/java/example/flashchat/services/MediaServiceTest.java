7e295b95afdc409dbbc781810ea5e117ce000df0
test1234d5x2 <moselsh1@gmail.com>
2025-06-24 21:24:30 +0100

    Service Tests Complete

---
+++ src/test/java/example/flashchat/services/MediaServiceTest.java
@@ -0,0 +1,72 @@
+package example.flashchat.services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import example.flashchat.models.Media;
+import example.flashchat.models.Post;
+import example.flashchat.models.User;
+import example.flashchat.repositories.MediaRepo;
+
+public class MediaServiceTest {
+    @Mock
+    private MediaRepo mediaRepo;
+
+    @InjectMocks
+    private MediaService mediaService;
+
+    private Media testMedia;
+    private Post testPost;
+    private User testUser;
+    
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        testUser = new User();
+        testUser.setUsername("testuser");
+        testUser.setPassword("password");
+
+        testPost = new Post();
+        testPost.setUser(testUser);
+        testPost.setPost("test post");
+        testPost.setDatePosted(new Date());
+
+        testMedia = new Media();
+        testMedia.setPost(testPost);
+        testMedia.setFilePath("test media url");
+    }
+
+    @Test
+    public void testAddMedia() {
+        when(mediaRepo.save(testMedia)).thenReturn(testMedia);
+        assertTrue(mediaService.addMedia(testMedia));
+    }
+
+    @Test
+    public void testDeleteMedia() {
+        when(mediaRepo.existsById(testMedia.getId())).thenReturn(true);
+        assertTrue(mediaService.deleteMedia(testMedia.getId()));
+    }
+
+    @Test
+    public void testMediaExists() {
+        when(mediaRepo.existsById(testMedia.getId())).thenReturn(true);
+        assertTrue(mediaService.mediaExists(testMedia.getId()));
+    }
+
+    @Test
+    public void testGetMedia() {
+        when(mediaRepo.findById(testMedia.getId())).thenReturn(Optional.of(testMedia));
+        assertTrue(mediaService.getMedia(testMedia.getId()) != null);
+    }
+}
